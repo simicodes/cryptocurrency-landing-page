7f65fe48159c9a84537e3af8fd074f802dd19b63
simicodes <simeonoffiong@gmail.com>
2022-08-05 13:56:44 +0100

    AccountBtns and Stats components updated

--- src/components/AccountBtns.js
+++ src/components/AccountBtns.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const AccountBtns = () => {
-  return <div>AccountBtns</div>;
+  return <div className='flex items-center font-medium'>
+    {/* link btn */}
+    <a className='hover:text-blue transition' href='#!'>Login</a>
+    {/* separator */}
+    <span className='mx-6 text-white/20 font-thin'>|</span>
+    {/* register btn */}
+    <button className='btn h-[52px] text-base px-8'>Register</button>
+  </div>;
 };
 
 export default AccountBtns;
